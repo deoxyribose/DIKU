a0d5e93b0da2be337880669bf1676db2581ba7c9
frans <frans.zdyb@deondigital.com>
2023-09-10 14:06:14 +0200

    1a and 1b work

--- Assignment1.js
+++ Assignment1.js
@@ -11,7 +11,7 @@ var AND = "AND";
 var NOT = "NOT";
 var ITE = "ITE";
 
-var ALLOPS = [NUM, FALSE, VR, PLUS, TIMES, LT, AND, NOT, ITE];
+var ALLOPS = [NUM, FALSE, VARIABLE, PLUS, TIMES, LT, AND, NOT, ITE];
 
 function str(obj) { return JSON.stringify(obj); }
 
@@ -188,177 +188,158 @@ function writeToConsole(text) {
     }
 }
 
-function bottomUp(globalBnd, intOps, boolOps, vars, consts, inputoutputs) {
-    // expr_types maps expressions to their type signatures
-    // the type signature of an expression is a list of the types of its arguments followed by the type of the expression
-    var exprNodes = { FALSE: False, NUM: Num, VARIABLE: Variable, PLUS: Plus, TIMES: Times, LT: Lt, AND: And, NOT: Not, ITE: Ite };;
-    function exprNode(expr) {
-        // if expr is in exprNodes
-        if (expr in exprNodes) {
-            return exprNodes[expr];
-        }
-        else {
-            return expr;
-        }
-    }
-    // for some reason the VAR key has to be VAR, not VR
-    var exprTypes = { FALSE: ["bool"], NUM: ["int"], VARIABLE: ["int"], PLUS: ["int", "int", "int"], TIMES: ["int", "int", "int"], LT: ["int", "int", "bool"], AND: ["bool", "bool", "bool"], NOT: ["bool", "bool"], ITE: ["bool", "int", "int", "int"] };;
-    function exprType(expr) {
-        expr = expr[0];
-        // if expr is in vars or consts
-        if (vars.includes(expr) || consts.includes(expr)) {
-            return ["int"];
-        }
-        else {
-            return exprTypes[expr];
-        }
-    }
+// expr_types maps expressions to their type signatures
+// the type signature of an expression is a list of the types of its arguments followed by the type of the expression
+let vars = ["x", "y"];
+let consts = [4, 5];
+let exprTypes = { FALSE: ["bool"], NUM: ["int"], VARIABLE: ["int"], PLUS: ["int", "int", "int"], TIMES: ["int", "int", "int"], LT: ["int", "int", "bool"], AND: ["bool", "bool", "bool"], NOT: ["bool", "bool"], ITE: ["bool", "int", "int", "int"] };;
+let exprNodes = { FALSE: False, NUM: Num, VARIABLE: Variable, PLUS: Plus, TIMES: Times, LT: Lt, AND: And, NOT: Not, ITE: Ite };;
 
-    function returnType(expr) {
-        return exprType(expr).slice(-1)[0];
+function exprNode(expr) {
+    // if expr is in exprNodes
+    if (expr in exprNodes) {
+        return exprNodes[expr];
     }
+    else {
+        return expr;
+    }
+}
+// for some reason the VAR key has to be VAR, not VR
+function exprType(expr, vars, consts) {
+    expr = expr[0];
+    // if expr is in vars or consts
+    if (vars.includes(expr) || consts.includes(expr)) {
+        return ["int"];
+    }
+    else {
+        return exprTypes[expr];
+    }
+}
 
-    // initial plist is the list of all terminals, i.e. variables, constants, and false
-    // i.e. concatentation of vars, consts, and [FALSE]
-    // but with all elements wrapped in singleton arrays
-    var plist = vars.map(x => [VARIABLE, [x]]).concat(consts.map(x => [NUM, [x]])).concat([[FALSE]]);
-    var nonterminals = [PLUS, TIMES, LT, AND, NOT, ITE];
+function returnType(expr, vars, consts) {
+    return exprType(expr, vars, consts).slice(-1)[0];
+}
 
-    function grow(plist, nonterminals) {
-        //     // this function applies every nonterminal to every possible combination of arguments from plist
-        //     // such that the type signature of the nonterminal matches the types of the arguments
-        var next_level_plist = [];
-        for (i = 0; i < nonterminals.length; i++) {
-            var nonterminal = nonterminals[i];
-            var argtypes = exprTypes[nonterminal].slice(0, -1);
-            var args = [];
-            for (j = 0; j < argtypes.length; j++) {
-                args.push(plist.filter(x => returnType(x) == argtypes[j]));
-            }
-            // console.log(args);
-            const cartesianProduct = args.reduce((accumulator, currentArray) => {
-                const newCombinations = [];
-                accumulator.forEach(existingCombination => {
-                    currentArray.forEach(newValue => {
-                        newCombinations.push(existingCombination.concat([newValue]));
-                    });
+// initial plist is the list of all terminals, i.e. variables, constants, and false
+// i.e. concatentation of vars, consts, and [FALSE]
+// but with all elements wrapped in singleton arrays
+
+function grow(plist, nonterminals, vars, consts) {
+    // this function applies every nonterminal to every possible combination of arguments from plist
+    // such that the type signature of the nonterminal matches the types of the arguments
+    var next_level_plist = [];
+    for (var i = 0; i < nonterminals.length; i++) {
+        var nonterminal = nonterminals[i];
+        var argtypes = exprTypes[nonterminal].slice(0, -1);
+        var args = [];
+        for (var j = 0; j < argtypes.length; j++) {
+            args.push(plist.filter(x => returnType(x, vars, consts) == argtypes[j]));
+        }
+        const cartesianProduct = args.reduce((accumulator, currentArray) => {
+            const newCombinations = [];
+            accumulator.forEach(existingCombination => {
+                currentArray.forEach(newValue => {
+                    newCombinations.push(existingCombination.concat([newValue]));
                 });
-                return newCombinations;
-            }, [[]]);
-
-            // Create next level productions by combining with the nonterminal
-            cartesianProduct.forEach(combination => {
-                next_level_plist.push([nonterminal, ...combination]);
             });
+            return newCombinations;
+        }, [[]]);
+
+        // Create next level productions by combining with the nonterminal
+        cartesianProduct.forEach(combination => {
+            next_level_plist.push([nonterminal, ...combination]);
+        });
+    }
+    return plist.concat(next_level_plist);
+}
+
+function applyToConstructor(constructor, argArray) {
+    var args = [null].concat(argArray);
+    var factoryFunction = constructor.bind.apply(constructor, args);
+    return new factoryFunction();
+}
+
+function tree_map(fn, arr) {
+    if (Array.isArray(arr)) {
+        return arr.map(item => tree_map(fn, item));
+    } else {
+        return fn(arr);
+    }
+}
+
+function AST(expr) {
+    return tree_map(x => exprNode(x), expr)
+}
+
+function compile(e) {
+    if (Array.isArray(e)) {
+        if (Object.values(exprNodes).includes(e[0])) {
+            // apply the constructor to the rest of the elements of expr
+            return applyToConstructor(e[0], compile(e.slice(1)));
         }
-        return plist.concat(next_level_plist);
-    }
-
-    function applyToConstructor(constructor, argArray) {
-        var args = [null].concat(argArray);
-        var factoryFunction = constructor.bind.apply(constructor, args);
-        return new factoryFunction();
-    }
-
-    function tree_map(fn, arr) {
-        if (Array.isArray(arr)) {
-            return arr.map(item => tree_map(fn, item));
-        } else {
-            return fn(arr);
-        }
-    }
-
-    function AST(expr) {
-        return tree_map(x => exprNode(x), expr)
-    }
-
-    function compile(e) {
-        if (Array.isArray(e)) {
-            if (Object.values(exprNodes).includes(e[0])) {
-                // apply the constructor to the rest of the elements of expr
-                return applyToConstructor(e[0], compile(e.slice(1)));
+        else {
+            if (e.length > 1) {
+                return e.map(x => compile(x));
             }
             else {
-                if (e.length > 1) {
-                    return e.map(x => compile(x));
-                }
-                else {
-                    // if first element of expr is not a constructor, then it is a variable or constant
-                    // so we return the first element of expr
-                    return compile(e[0]);
-                }
+                // if first element of expr is not a constructor, then it is a variable or constant
+                // so we return the first element of expr
+                return compile(e[0]);
             }
+        }
+    }
+    else {
+        return e
+    }
+}
+
+function elimEquvalents(plist, inputoutputs) {
+    let obsUniqs = {};
+    for (var p of plist) {
+        var ast = compile(AST(p));
+        let outs = inputoutputs.map(x => ast.interpret(x));
+        if (outs in obsUniqs) {
+            continue;
         }
         else {
-            return e
-        }
-    }
-
-
-    // function elimEquvalents(plist, inputoutputs) {
-    //     // for each inputoutput, for each expression in plist,  interpret the plist expression with the inputoutput
-    //     // if the output is not in outputs, then push it to outputs
-    //     // if the output is in outputs, then remove the expression from plist
-    //     for (i = 0; i < inputoutputs.length; i++) {
-    //         var outputs = [];
-    //         var envt = inputoutputs[i];
-    //         for (j = 0; j < plist.length; j++) {
-    //             var expr = plist[j];
-    //             var output = compile(AST(expr)).interpret(envt);
-    //             console.log(output);
-    //             if (outputs.includes(output)) {
-    //                 // remove expr from plist
-    //                 plist.splice(j, 1);
-    //                 j--;
-    //                 // break
-    //             }
-    //             else {
-    //                 outputs.push(output);
-    //             }
-    //             console.log(outputs);
-    //         }
-    //     }
-    //     return plist;
-    // }
-
-    function elimEquvalents(plist, inputoutputs) {
-        let obsUniqs = {};
-        for (p of plist) {
-            ast = compile(AST(p));
-            console.log(ast);
-            let outs = inputoutputs.map(x => ast.interpret(x));
             obsUniqs[outs] = p;
         }
-        plist = Object.values(obsUniqs);
-        return plist
     }
+    plist = Object.values(obsUniqs);
+    return plist
+}
 
-    // var foo = compile(AST(plist[30]));
-    // foo;
-    // console.log(foo.interpret({ x: 5, y: 10, _out: 5 }));
+// var foo = compile(AST(plist[30]));
+// foo;
+// console.log(foo.interpret({ x: 5, y: 10, _out: 5 }));
 
-    function isCorrect(p, inputoutputs) {
-        for (i = 0; i < inputoutputs.length; i++) {
-            var envt = inputoutputs[i];
-            var output = compile(AST(p)).interpret(envt);
-            console.log(output);
-            if (output != envt._out) {
-                return false;
-            }
+function isCorrect(p, inputoutputs) {
+    var ast = compile(AST(p));
+    let outs = inputoutputs.map(x => ast.interpret(x));
+    // compare outs to _out
+    for (var i = 0; i < outs.length; i++) {
+        if (outs[i] != inputoutputs[i]._out) {
+            return false;
         }
-        return true;
     }
+    console.log(outs);
+    return true; 
+}
+
+function bottomUp(globalBnd, intOps, boolOps, vars, consts, inputoutput) {
+    var plist = vars.map(x => [VARIABLE, [x]]).concat(consts.map(x => [NUM, [x]])).concat([[FALSE]]);
+    var nonterminals = [PLUS, TIMES, LT, AND, NOT, ITE];
     console.log(plist);
-    for (c = 0; c < globalBnd; c++) {
+    for (var c = 0; c < globalBnd; c++) {
         console.log(c);
-        plist = grow(plist, nonterminals);
+        plist = grow(plist, nonterminals, vars, consts);
         console.log(plist.length);
-        plist = elimEquvalents(plist, inputoutputs);
+        plist = elimEquvalents(plist, inputoutput);
         console.log(plist.length);
-        for (n = 0; n < plist.length; n++) {
+        for (var n = 0; n < plist.length; n++) {
             var p = plist[n];
-            if (isCorrect(p, inputoutputs)) {
-                return AST(p);
+            if (isCorrect(p, inputoutput)) {
+                return compile(AST(p));
             }
         }
         console.log(p);
@@ -372,29 +353,90 @@ function bottomUp(globalBnd, intOps, boolOps, vars, consts, inputoutputs) {
 }
 
 
-// correct_p = ["ITE", ["LT", ["VARIABLE", ["x"]], ["VARIABLE", ["y"]]], ["VARIABLE", ["x"]], ["VARIABLE", ["y"]]];
-// isCorrect(correct_p, inputoutputs);
+// var correct_p = ["ITE", ["LT", ["VARIABLE", ["x"]], ["VARIABLE", ["y"]]], ["VARIABLE", ["x"]], ["VARIABLE", ["y"]]];
+// isCorrect(correct_p, inputoutputs); 
 
 // var rv = bottomUp(3, [VARIABLE, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [4, 5], [{ x: 5, y: 10, _out: 5 }, { x: 8, y: 3, _out: 3 }]);
-var rv = bottomUp(2, [VARIABLE, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [4, 5], [{ x: 5, y: 10, _out: 5 }, { x: 8, y: 3, _out: 3 }]);
-rv;
+// var rv = bottomUp(2, [VARIABLE, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [4, 5], [{ x: 5, y: 10, _out: 5 }, { x: 8, y: 3, _out: 3 }]);
+// rv;
+
+function get_arguments(nonterminal, plist, vars, consts, argtypes) {
+    var args = [];
+    var vars = vars.map(x => [VARIABLE, [x]]);
+    var var_and_consts = vars.concat(consts.map(x => [NUM, [x]]));
+    if (nonterminal == TIMES) {
+        // Multiplications can only occur between variables and constants or between two variables
+        args = [vars, var_and_consts];
+    }
+    else if (nonterminal == LT) {
+        // Comparisons cannot include any arithmetic, only variables and constants
+        args = [var_and_consts, var_and_consts];        
+    }
+    else {
+        // Just get all the arguments of the correct type
+        for (var j = 0; j < argtypes.length; j++) {
+            args.push(plist.filter(x => returnType(x, vars, consts) == argtypes[j]));
+        }
+    }
+    return args;
+}
+    
+
+function growFaster(plist, nonterminals, vars, consts) {
+    // this function applies every nonterminal to every possible combination of arguments from plist
+    // such that the type signature of the nonterminal matches the types of the arguments
+    var next_level_plist = [];
+    for (var i = 0; i < nonterminals.length; i++) {
+        var nonterminal = nonterminals[i];
+        var argtypes = exprTypes[nonterminal].slice(0, -1);
+        var args = get_arguments(nonterminal, plist, vars, consts, argtypes);
+        const cartesianProduct = args.reduce((accumulator, currentArray) => {
+            const newCombinations = [];
+            accumulator.forEach(existingCombination => {
+                currentArray.forEach(newValue => {
+                    newCombinations.push(existingCombination.concat([newValue]));
+                });
+            });
+            return newCombinations;
+        }, [[]]);
+
+        // Create next level productions by combining with the nonterminal
+        cartesianProduct.forEach(combination => {
+            next_level_plist.push([nonterminal, ...combination]);
+        });
+    }
+    return plist.concat(next_level_plist);
+}
 
 function bottomUpFaster(globalBnd, intOps, boolOps, vars, consts, inputoutput) {
-
-    return "NYI";
+    var plist = vars.map(x => [VARIABLE, [x]]).concat(consts.map(x => [NUM, [x]])).concat([[FALSE]]);
+    var nonterminals = [PLUS, TIMES, LT, AND, NOT, ITE];
+    console.log(plist);
+    for (var c = 0; c < globalBnd; c++) {
+        console.log(c);
+        plist = growFaster(plist, nonterminals, vars, consts);
+        console.log(plist.length);
+        plist = elimEquvalents(plist, inputoutput);
+        console.log(plist.length);
+        for (var n = 0; n < plist.length; n++) {
+            var p = plist[n];
+            if (isCorrect(p, inputoutput)) {
+                return compile(AST(p));
+            }
+        }
+        console.log(p);
+    }
+    return "FAIL";
 }
 
 
 function run1a1() {
-
     var rv = bottomUp(3, [VARIABLE, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [4, 5], [{ x: 5, y: 10, _out: 5 }, { x: 8, y: 3, _out: 3 }]);
     writeToConsole("RESULT: " + rv.toString());
 }
 
-
 function run1a2() {
-
-    var rv = bottomUp(3, [VR, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [-1, 5], [
+    var rv = bottomUp(3, [VARIABLE, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [-1, 5], [
         { x: 10, y: 7, _out: 17 },
         { x: 4, y: 7, _out: -7 },
         { x: 10, y: 3, _out: 13 },
@@ -408,7 +450,7 @@ function run1a2() {
 
 function run1b() {
 
-    var rv = bottomUpFaster(3, [VR, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [-1, 5], [
+    var rv = bottomUpFaster(3, [VARIABLE, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [-1, 5], [
         { x: 10, y: 7, _out: 17 },
         { x: 4, y: 7, _out: -7 },
         { x: 10, y: 3, _out: 13 },
